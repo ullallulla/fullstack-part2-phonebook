4eb68cf5ef50a19d1d84b1a5a07e473424642731
ullallulla <aleksi.ojansivu@borealfoundry.com>
2021-03-02 12:02:01 +0200

    added update functionality and changed the filter to show all data when filter text field has no input value

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -4,7 +4,7 @@ import Person from './Person'
 
 const Persons = ({filter, persons, deletePerson}) => {
 
-  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()) && filter != '')
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()) )
   return (
     personsToShow.map((person, i) => 
       <Person key={i} person={person} deletePerson={() => deletePerson(person.id)}/>
